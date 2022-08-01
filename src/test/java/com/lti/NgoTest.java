543df1d93eca22d23d9f5b88d9cdc6ea4d29a0a8
JayArya <spice.arya@gmail.com>
2022-08-01 16:43:43 +0530

    Fix import in Ngo Test

--- src/test/java/com/lti/NgoTest.java
+++ src/test/java/com/lti/NgoTest.java
@@ -1,8 +1,5 @@
 package com.lti;
 
-import static org.junit.Assert.*;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
