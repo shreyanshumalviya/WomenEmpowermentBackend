9944eb36d81db3a9325245d844e2759f6d742694
Shreyanshu <shreyanshumalviya@gmail.com>
2022-08-02 13:23:10 +0530

    sukanya doc uploading, refering to user

--- src/main/java/com/lti/controller/SukanyaController.java
+++ src/main/java/com/lti/controller/SukanyaController.java
@@ -7,9 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lti.dto.NgoDocRegister;
+import com.lti.dto.SukanyaDocRegister;
 import com.lti.dto.SukanyaRegistrationDto;
 import com.lti.entity.Ngo;
+import com.lti.entity.NgoDocuments;
 import com.lti.entity.SukanyaAccount;
+import com.lti.entity.SukanyaDoc;
 import com.lti.service.NgoServiceImpl;
 import com.lti.service.SukanyaServiceImpl;
 
@@ -25,7 +29,12 @@ public class SukanyaController {
 	public SukanyaAccount signup(@RequestBody SukanyaRegistrationDto sukanyaAccount) {
 		System.out.println("here");
 
-		return sukanyaService.register(sukanyaAccount.toSukanyaAccount());
+		return sukanyaService.register(sukanyaAccount.toSukanyaAccount(), sukanyaAccount.getUserId());
 	}	
+
+	@RequestMapping(value = "/upload_doc", method = RequestMethod.POST)
+	public SukanyaDoc postNgoDocument(@RequestBody SukanyaDocRegister docRegister) {
+		return sukanyaService.postSukanyaDocument(docRegister);
+	}
 	
 }
--- src/main/java/com/lti/service/SukanyaService.java
+++ src/main/java/com/lti/service/SukanyaService.java
@@ -1,8 +1,12 @@
 package com.lti.service;
 
+import com.lti.dto.SukanyaDocRegister;
 import com.lti.entity.SukanyaAccount;
+import com.lti.entity.SukanyaDoc;
 
 public interface SukanyaService {
-	SukanyaAccount register(SukanyaAccount sukanyaAccount);
+	SukanyaAccount register(SukanyaAccount sukanyaAccount, int userId);
+
+	SukanyaDoc postSukanyaDocument(SukanyaDocRegister docRegister);
 
 }
